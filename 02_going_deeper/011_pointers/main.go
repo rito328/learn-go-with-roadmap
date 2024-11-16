ff28ac8136832d273c825eb90917c1129911766e
rito328 <ritolabo@gmail.com>
2024-11-16 21:58:24 +0900

    added 011_pointers

---
+++ 02_going_deeper/011_pointers/main.go
@@ -0,0 +1,62 @@
+package main
+
+import "fmt"
+
+func main() {
+}
+
+// -- ポインターの基本概念 -- //
+func main01() {
+	// 通常の変数
+	x := 10
+
+	// ポインター変数の宣言 (*int は int型を指すポインター)
+	var p *int
+
+	// x のアドレスを p に格納
+	p = &x
+
+	// ポインターを通じて x の値を変更
+	*p = 20
+
+	fmt.Printf("x の値: %d\n", x)           // 20
+	fmt.Printf("p の値(アドレス): %v\n", p) // メモリアドレス
+	fmt.Printf("p の指す値: %d\n", *p)      // 20
+}
+
+// -- ポインターを使った値の変更 -- //
+func main02() {
+	x := 42
+	p := &x
+	*p = 100       // ポインターを通じて値を変更
+	fmt.Println(x) // 100
+}
+
+// -- 関数とポインタ -- //
+func updateValue(p *int) {
+	*p = 99 // ポインターを使って値を更新
+}
+func main03() {
+	x := 42
+	updateValue(&x) // x のアドレスを渡す
+	fmt.Println(x)  // 99
+}
+
+// -- 構造体とポインタ -- //
+type Person struct {
+	Name string
+	Age  int
+}
+
+func (p *Person) Birthday() {
+	p.Age++ // (*p).Age++ と同じ
+}
+func main04() {
+	person := &Person{
+		Name: "田中",
+		Age:  25,
+	}
+	person.Birthday()
+	fmt.Printf("年齢: %d\n", person.Age) // 26
+
+}
